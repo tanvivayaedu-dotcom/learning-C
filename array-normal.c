bd34511c33ae8d15efaf22638c06794de1d0f3f2
tanvivayaedu-dotcom <tanvi.vaya.edu@gmail.com>
2025-11-15 23:06:34 +0530

    Create array-normal.c

---
+++ array-normal.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr [5]={5,10,15,20,25};
+     
+    for(int i=0; i<5; i ++)
+    {
+        printf("the elements of the array are: %d\n", arr[i]);
+    }
+
+return 0;
+}
+
