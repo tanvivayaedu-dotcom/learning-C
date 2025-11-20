d80ee9cc78412f88672a3c26c4b00b8bd47def8f
Tanvi Vaya <tanvi.vaya.edu@gmail.com>
2025-11-20 22:17:30 +0530

    Create dowhile.c

---
+++ dowhile.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main ()
+{
+    
+    
+    char *name = "Tanvi";
+    
+    int iCount = 0;
+  do{
+      printf("%s\n",name);
+      iCount++;
+  }  while (iCount < 5);
+  //  {
+   //     printf("%s \n", name);
+   //     iCount++;
+   // }
+    
+    return 0;
+
+}
